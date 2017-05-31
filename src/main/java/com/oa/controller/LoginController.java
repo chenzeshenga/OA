6e4e04329051147467bc224aa86e05e351b424cf
xiongshengjie <1145421439@qq.com>
2017-05-31 14:07:56 +0800

    change Spring version to 4.3.8.RELEASE

--- src/main/java/com/oa/controller/LoginController.java
+++ src/main/java/com/oa/controller/LoginController.java
@@ -1,7 +1,8 @@
 package com.oa.controller;
 
 /**
- * Created by Administrator on 2017/5/31.
+ * Created by xiongshengjie on 2017/5/31.
  */
+
 public class LoginController {
 }
--- src/main/java/com/oa/service/UserService.java
+++ src/main/java/com/oa/service/UserService.java
@@ -1,7 +1,7 @@
 package com.oa.service;
 
 /**
- * Created by Administrator on 2017/5/31.
+ * Created by xiongshengjie on 2017/5/31.
  */
 public class UserService {
 }
